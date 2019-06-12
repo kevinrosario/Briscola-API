23e18cf652592134631f2d503de2ee4944599191
kevinrosario <kevin.rosario9@upr.edu>
2019-06-12 13:35:51 -0400

    Remove added tags after merge conflict

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 class GameSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :briscola, :player_one_hand, :player_one_earned,
-             :current_cards, :deck, :over
-
-=======
   attributes :id, :briscola, :player_one_hand, :current_cards, :deck,
              :player_one_earned, :player_two_last_selection, :over
->>>>>>> development
   has_one :user
 end
